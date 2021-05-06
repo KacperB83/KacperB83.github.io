3c9ab327a004f1f9bad36ac93ceb4a681b3a0408
Kacper Bojarski <k.bojarski.pl@gmail.com>
2021-05-06 14:47:25 +0200

    zmiana w script na heroku

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'http://localhost:8080/v1/task/';
+   const apiRoot = 'http://infinite-sierra-60690.herokuapp.com/v1/task/getTasks';
    const trelloApiRoot = 'http://localhost:8080/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
