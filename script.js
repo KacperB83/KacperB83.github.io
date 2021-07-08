cd1c0e5567ab7ec86874aab2bacb117650932634
Kacper Bojarski <k.bojarski.pl@gmail.com>
2021-07-08 10:37:26 +0200

    heroku root to trello

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://infinite-sierra-60690.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const trelloApiRoot = 'https://infinite-sierra-60690.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
